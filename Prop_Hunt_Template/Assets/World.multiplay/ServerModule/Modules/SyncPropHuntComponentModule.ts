729462b36240625d4e68a858f973e60b3f20a75e
Hernan A. Ferrari <hernan.ferrari@captivatar.com>
2023-09-11 12:56:45 -0300

    Added test case in Sync

--- Prop_Hunt_Template/Assets/World.multiplay/ServerModule/Modules/SyncPropHuntComponentModule.ts
+++ Prop_Hunt_Template/Assets/World.multiplay/ServerModule/Modules/SyncPropHuntComponentModule.ts
@@ -43,22 +43,22 @@ export default class SyncPropHuntComponentModule extends IModule {
             let allPlayersReady : boolean;
             allPlayersReady = this.playerDataModelCaches.every((playerCache) => playerCache.isReady == true);
 
-            let anyHunter : boolean = false;
-            let anyProp: boolean = false;
+            // let anyHunter : boolean = false;
+            // let anyProp: boolean = false;
             
-            this.playerDataModelCaches.forEach(element => {
-                if(element.isHunter == true)
-                {
-                    anyHunter = true;
-                }
-                else
-                {
-                    anyProp = true;
-                }
-            });
+            // this.playerDataModelCaches.forEach(element => {
+            //     if(element.isHunter == true)
+            //     {
+            //         anyHunter = true;
+            //     }
+            //     else
+            //     {
+            //         anyProp = true;
+            //     }
+            // });
 
 
-            if(allPlayersReady && anyHunter && anyProp)
+            if(allPlayersReady) // && anyHunter && anyProp)
             {
                 console.log("StartGame: " + allPlayersReady);
                 this.server.broadcast(GAME_MESSAGE.OnStartGameArrived, "");
