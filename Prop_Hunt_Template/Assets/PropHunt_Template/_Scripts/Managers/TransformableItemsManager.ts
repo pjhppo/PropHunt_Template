c4082b119abe03efed102b99ba69dadd5f4299ca
Ilian <ilian.ostapovich@captivatar.com>
2023-09-11 12:59:37 -0300

    Init the buttons for prop

--- Prop_Hunt_Template/Assets/PropHunt_Template/_Scripts/Managers/TransformableItemsManager.ts
+++ Prop_Hunt_Template/Assets/PropHunt_Template/_Scripts/Managers/TransformableItemsManager.ts
@@ -11,40 +11,46 @@ export default class TransformableItemsManager extends ZepetoScriptBehaviour {
     public static instance: TransformableItemsManager;
 
     public uiTransformableButtonTemplate: GameObject;
-    public transformableButtonsParent : Transform;
-    public itemsTransformablesSO : ZepetoScriptableObject<Itemtransformable>[] = [];
+    public transformableButtonsParent: Transform;
+    public itemsTransformablesSO: ZepetoScriptableObject<Itemtransformable>[] = [];
 
     private _allItemsTransformables: Map<string, GameObject> = new Map<string, GameObject>();
 
-    Awake(){
+    Awake() {
         if (TransformableItemsManager.instance != null) GameObject.Destroy(this.gameObject);
         else TransformableItemsManager.instance = this;
     }
 
-    Start() {    
+    Start() {
         this.InstantiateButtons();
     }
 
-    public InstantiateButtons()
-    {
+    public InstantiateButtons() {
         this.itemsTransformablesSO.forEach(element => {
-            let newUITransformableButton : UITransformableButton = GameObject.Instantiate(this.uiTransformableButtonTemplate, this.transformableButtonsParent) as UITransformableButton;
+            let newUITransformableButton: UITransformableButton = GameObject.Instantiate(this.uiTransformableButtonTemplate, this.transformableButtonsParent) as UITransformableButton;
             newUITransformableButton.GetComponent<UITransformableButton>().SetButton(element.targetObject);
-            let newObjectInGame : GameObject = GameObject.Instantiate(element.targetObject.itemPrefab, this.transform) as GameObject;
+            // let newObjectInGame: GameObject = GameObject.Instantiate(element.targetObject.itemPrefab, this.transform) as GameObject;
 
-            newObjectInGame.SetActive(false);
+            // newObjectInGame.SetActive(false);
 
-            this._allItemsTransformables.set(element.targetObject.itemId, newObjectInGame);
+            // this._allItemsTransformables.set(element.targetObject.itemId, newObjectInGame);
         });
     }
 
-    public TransformPlayer(itemId : string, sessionId: string)
-    {
+    public TransformPlayer(itemId: string, sessionId: string) {
+        if (this._allItemsTransformables.has(itemId)) {
+
+            // let newObjectInGame: GameObject = GameObject.Instantiate(element.targetObject.itemPrefab, this.transform) as GameObject;
+
+            // newObjectInGame.SetActive(false);
+
+            // this._allItemsTransformables.set(itemId, newObjectInGame);
+        }
+
         let tempItemTransfromablethis = this._allItemsTransformables.get(itemId);
         let playerData = GameManager.instance.GetPlayer(sessionId);
-    
-        if(tempItemTransfromablethis && !playerData.isHunter)
-        {   
+
+        if (tempItemTransfromablethis && !playerData.isHunter) {
             const zepetoPlayer = ZepetoPlayers.instance.GetPlayer(sessionId).character.gameObject;
             zepetoPlayer.GetComponent<NonHunterController>().TransformIntoItem(tempItemTransfromablethis);
             tempItemTransfromablethis.SetActive(true);
@@ -53,12 +59,10 @@ export default class TransformableItemsManager extends ZepetoScriptBehaviour {
         }
     }
 
-    public TransformLocalPlayer(itemId : string)
-    {
+    public TransformLocalPlayer(itemId: string) {
         let tempItemTransfromablethis = this._allItemsTransformables.get(itemId);
-        
-        if(tempItemTransfromablethis)
-        {
+
+        if (tempItemTransfromablethis) {
             MultiplayerPropHuntManager.instance.ChangeItem(itemId);
         }
     }
